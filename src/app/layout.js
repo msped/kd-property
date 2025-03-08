7b9a5721df6d30ae1b4e59e95c4e2945d65b397e
Matt Edwards <mattsp.edwards@googlemail.com>
2025-03-08 10:43:54 +0000

    fix: font variable declaration and theme import

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Roboto } from "next/font/google";
 import { ThemeProvider } from '@mui/material/styles';
-import theme from '../theme';
+import theme from '@/theme';
 import "./globals.css";
 
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
@@ -21,7 +21,7 @@ export default function RootLayout(props) {
   const { children } = props;
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${roboto.variable}`}>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
             {children}
