3704d4ca9062aee000f3da302cad74f16ce14fc3
Matt Edwards <mattsp.edwards@googlemail.com>
2025-03-17 21:30:21 +0000

    build: installed jest and config file

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require('next/jest')
+
+/** @type {import('jest').Config} */
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+}
+
+module.exports = createJestConfig(config)
