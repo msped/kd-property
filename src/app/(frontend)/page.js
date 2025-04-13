a735940c9b02e291a040913e379c116c0235b3b8
Matt Edwards <mattsp.edwards@googlemail.com>
2025-04-14 00:00:09 +0100

    feat: added carsouel to home page route

--- src/app/(frontend)/page.js
+++ src/app/(frontend)/page.js
@@ -1,8 +1,42 @@
+import { getPayload } from 'payload';
+import config from '@/payload.config';
+import Carsouel from "@/app/(frontend)/components/Carousel";
+import { Typography } from '@mui/material';
+import { Stack } from '@mui/material';
+import { Button } from '@mui/material';
+import { Box } from '@mui/material';
 
-export default function Home() {
+export const metadata = {
+  title: 'KD Property',
+  description: ''
+}
+
+export default async function Home() {
+
+  const payload = await getPayload({ config });
+
+  const { docs: images } = await payload.find({
+      collection: 'hero-images',
+      depth: 1,
+  });
+
   return (
-    <div>
-      
-    </div>
+    <Box sx={{
+        position: 'relative',
+        zIndex: 0,
+        width: '100%',
+      }}
+    >
+      <Carsouel images={images[0].images}>
+        <Stack direction='column' justifyContent='center' alignItems='center' spacing={2}>
+          <Typography variant='h3' component='h1' fontWeight={600} color='white'>
+            KD Property Development
+          </Typography>
+          <Button href='/contact' variant='contained'>
+            Build your dream
+          </Button>
+        </Stack>
+      </Carsouel>
+    </Box>
   );
 }
